096e15e144a5561636528153d7463f9edbca4988
ahegde3 <ahegde3@outlook.com>
2021-01-14 16:46:43 +0530

    multer added

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var express=require("express")
 var app=express()
 var vision=require('@google-cloud/vision')
+const { checkError } = require('./multerLogic')
 var port=3030
 
-async function detection()
-{
-    const client = new vision.ImageAnnotatorClient();
-  
+
+app.use(express.json())
+async function detection(req,res)
+{ try{
+    //const client = new vision.ImageAnnotatorClient();
+   
     // Performs label detection on the image file
-    const [result] = await client.labelDetection('wakeup.jpg');
-    const labels = result.labelAnnotations;
-    console.log('Labels:');
-    //console.log(labels)
-    labels.forEach(label => console.log(label.description));
+    // const [result] = await client.labelDetection('wakeup.jpg');
+    // const labels = result.labelAnnotations;
+    // console.log('Labels:');
+    // //console.log(labels)
+    // labels.forEach(label => console.log(label.description));
+
+    const client=new vision.ImageAnnotatorClient()
+    const imageDesc = await checkError(req, res)
+        console.log(imageDesc)
+ 
+    const [result]=await client.textDetection(imageDesc.path)
+    const detections=result.textAnnotations
+    const [ text, ...others ] = detections
+    res.send(`Text: ${ text.description }`)
+      }
+    catch (error) {console.log(error)}  
 }
 
 
